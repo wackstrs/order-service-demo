f86681315e84723e8e2f707955a3f0718d04c908
wackstrs <108585225+wackstrs@users.noreply.github.com>
2025-01-18 16:00:36 +0200

    test

--- src/server.js
+++ src/server.js
@@ -11,19 +11,21 @@ console.log(`Node.js ${process.version}`)
 
 app.use(express.json())
 
+// Root endpoint to check database connection
 app.get('/', async (req, res) => {
     try {
         // Try to fetch the current time from the database to ensure the connection is working
-        const result = await prismaClient.$queryRaw`SELECT NOW()`;
+        const result = await prisma.$queryRaw`SELECT NOW()`; // Corrected to use "prisma"
         res.json({ message: 'Database connection successful', result });
-      } catch (error) {
+    } catch (error) {
         res.status(500).json({ message: 'Database connection failed', error: error.message });
-      }
-    });
+    }
+});
 
+// Endpoint to test fetching data using Prisma
 app.get('/prisma', async (req, res) => {
     const test = await prisma.test.findMany()
-    res.send({ msg: 'mongodb prisma findMany', test: test })
+    res.send({ msg: 'Prisma findMany', test: test })
 })
 
 // Default health check endpoint
@@ -36,11 +38,11 @@ app.get('/health', (req, res) => {
     })
 })
 
+// Start the server
 app.listen(PORT, () => {
     try {
         console.log(`Running on http://localhost:${PORT}`)
     } catch (error) {
-        res.status(500).json({ message: error.message })
+        console.log(error.message);
     }
-    
 })
